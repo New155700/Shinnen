8aa78a255b971a81d7580bf8b219ae423311a9e7
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 14:58:23 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,13 +1,16 @@
 local currentId = game.PlaceId
 local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
+
 local MapScripts = {
-    [85638494463963] = "Games1.lua",
-    [96255502718881] = "Games2.lua",
+    [85638494463963] = "Games1.lua", -- แมพเก่า
+    [96255502718881] = "Games2.lua", -- แมพ DUEL Warriors (เลขจากรูป 5165)
 }
 
 if MapScripts[currentId] then
-    print("Shinnen Hub: Loading " .. MapScripts[currentId])
-    loadstring(game:HttpGet(baseUrl .. MapScripts[currentId]))()
+    local fileName = MapScripts[currentId]
+    loadstring(game:HttpGet(baseUrl .. fileName))()
 else
-    print("Shinnen Hub: ID NOT FOUND -> " .. tostring(currentId))
+    -- ถ้าไม่เจอไอดี ไม่ต้องเตะ! ให้มันบอกเลขมาเลยจะได้แก้ถูก
+    warn("Shinnen Hub: ไม่พบไอดีนี้ -> " .. tostring(currentId))
 end
+nd
