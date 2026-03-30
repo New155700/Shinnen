a86d2e708b12f5d6f96e792ad7fe17dbdc0d9ce1
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 07:19:15 +0700

    load.lua

---
+++ load.lua
@@ -0,0 +1,12 @@
+repeat task.wait() until game:IsLoaded()
+
+local BaseURL = "https://raw.githubusercontent.com/New155700/Shinnen/main/Games/"
+local ScriptURL = BaseURL .. game.PlaceId .. ".lua"
+
+local ok,res = pcall(game.HttpGet, game, ScriptURL)
+
+if ok and res and not res:find("404") then
+    loadstring(res)()
+else
+    warn("Game not supported")
+end
