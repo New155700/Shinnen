257663df85356038f48a243c79158314cd3b49cd
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 15:19:00 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,24 +1,13 @@
--- [[ SHINNEN HUB | FINAL DEBUG ]] --
-print("Starting Shinnen Hub...")
+local MapScripts = {
+    [85638494463963] = "Games1.lua",
+    [96255502718881] = "Games2.lua"
+}
 
-local success, result = pcall(function()
-    local currentId = game.PlaceId
-    local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
-    local MapScripts = {
-        [85638494463963] = "Games1.lua",
-        [96255502718881] = "Games2.lua",
-    }
+local scriptName = MapScripts[game.PlaceId]
 
-    if MapScripts[currentId] then
-        local fileName = MapScripts[currentId]
-        print("Found Map! Loading: " .. fileName)
-        loadstring(game:HttpGet(baseUrl .. fileName))()
-    else
-        game.Players.LocalPlayer:Kick("ID NOT FOUND: " .. tostring(currentId))
-    end
-end)
-
-if not success then
-    print("FATAL ERROR: " .. tostring(result))
-    warn("สคริปต์พังเพราะ: " .. tostring(result))
+if scriptName then
+    local url = "https://raw.githubusercontent.com/New155700/Shinnen/main/" .. scriptName
+    loadstring(game:HttpGet(url))()
+else
+    game.Players.LocalPlayer:Kick("ไม่รองรับแมพนี้ไอดี: " .. tostring(game.PlaceId))
 end
