12568d7d295cadc402796e8f18e14155f04c14ad
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 08:45:41 +0700

    load.lua
    Update ScriptURL to use a specific PlaceId instead of game.PlaceId.

--- load.lua
+++ load.lua
@@ -1,6 +1,6 @@
 
 local BaseURL = "https://raw.githubusercontent.com/New155700/Shinnen/main/Games/"
-local ScriptURL = BaseURL .. game.PlaceId .. ".lua"
+local ScriptURL = BaseURL .. 96255502718881.PlaceId .. ".lua"
 
 local ok,res = pcall(game.HttpGet, game, ScriptURL)
 
