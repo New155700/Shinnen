a2e3f95d712a58fc93b8b0b0155e5478b6ce6888
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 17:19:02 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,15 +1,19 @@
-local Scripts = {
+-- [[ SHINNEN HUB | 100% WORKING LOADER ]] --
+local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
+local currentId = game.PlaceId
+
+local MapScripts = {
     [85638494463963] = "Games1.lua",
     [96255502718881] = "Games2.lua"
 }
 
-local currentId = game.PlaceId
-local fileName = Scripts[currentId]
-local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
+local fileName = MapScripts[currentId]
 
 if fileName then
+    -- ถ้าเจอไอดี ให้โหลดไฟล์นั้นทันที
     loadstring(game:HttpGet(baseUrl .. fileName))()
 else
-    -- ถ้าหาไอดีไม่เจอ ให้เด้งออกและบอกเลขไอดีมาเลย จะได้แก้ถูก
-    game.Players.LocalPlayer:Kick("ID NOT FOUND: " .. tostring(currentId))
+    -- ถ้าไม่เจอไอดี ให้บังคับโหลด Games1.lua ไปก่อน (กันเหนียว)
+    warn("ID Not Found ("..tostring(currentId).."), Force Loading Games1")
+    loadstring(game:HttpGet(baseUrl .. "Games1.lua"))()
 end
