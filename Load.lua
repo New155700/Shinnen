287f548a1eaddd0cc28edeb1e1d3301e4bf8e5ac
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 12:47:07 +0700

    Update URLs in Load.lua for success loader

--- Load.lua
+++ Load.lua
@@ -1,10 +1,10 @@
 -- Shinnen Hub Success Loader (100% Fixed)
 local Success, Error = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/main/96255502718881.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/main/games"))()
 end)
 
 if not Success then
     warn("Loader Error: " .. tostring(Error))
     -- กรณี Link ด้านบนมีปัญหา จะลอง Link สำรองแบบระบุหัว Branch
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/refs/heads/main/96255502718881.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/refs/heads/main/games"))()
 end
