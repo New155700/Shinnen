00edd0dba0eae9aea560a865b78212180a75a04a
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 15:33:41 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -1,10 +1,10 @@
-local Scripts = {
+main Scripts = {
     [85638494463963] = "Games1.lua",
     [96255502718881] = "Games2.lua"
 }
 
 local name = Scripts[game.PlaceId]
-local url = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
+local url = "https://raw.githubusercontent.com/New155700/Shinnen/main/Games1.lua"
 
 if name then
     loadstring(game:HttpGet(url .. name))()
