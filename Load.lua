751519538c029a2e512f833ba225730857424dca
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 12:50:36 +0700

    Fix URL for loading games in Load.lua

--- Load.lua
+++ Load.lua
@@ -1,10 +1,10 @@
 -- Shinnen Hub Success Loader (100% Fixed)
 local Success, Error = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/main/games"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/main/Games.lua"))()
 end)
 
 if not Success then
     warn("Loader Error: " .. tostring(Error))
     -- กรณี Link ด้านบนมีปัญหา จะลอง Link สำรองแบบระบุหัว Branch
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/refs/heads/main/games"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/New155700/Shinnen/refs/heads/main/Games.lua"))()
 end
