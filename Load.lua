2596c4816623c75d9a0407337c7fc9e1dbf2062d
New155700 <120209183+New155700@users.noreply.github.com>
2026-03-30 17:27:14 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -3,7 +3,7 @@ local currentId = game.PlaceId
 local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
 
 local MapScripts = {
-    [85638494463963] = "Games1.lua",
+    [100400297022629] = "Games1.lua",
     [96255502718881] = "Games2.lua"
 }
 
