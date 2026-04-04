46e64311c9934bc2cdfbace29a684a312c038fe5
New155700 <120209183+New155700@users.noreply.github.com>
2026-04-04 16:34:28 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -8,7 +8,7 @@ local baseUrl = "https://raw.githubusercontent.com/New155700/Shinnen/main/"
 -- [ 📋 ตารางแมพ (ล็อค ID ให้ตรงไฟล์) ]
 local MapConfig = {
     [142823291] = "Games2.lua", -- ID แมพ Murder Mystery 2 หลัก
-    [14469379009] = "Games2.lua", -- ID แมพทดสอบที่พี่ใช้
+    [14469379009] = "Games3.lua", -- ID แมพทดสอบที่พี่ใช้
 }
 
 -- ระบบล้าง UI เก่าป้องกันบัค
